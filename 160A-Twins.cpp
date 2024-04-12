e175444ffbf79b0e85d4c1f9ae1569950a6c4a7e
MD zahid hasan <114461735+Zahid404Mars@users.noreply.github.com>
2024-04-12 19:53:47 +0600

    Create 160A-Twins.cpp

---
+++ 160A-Twins.cpp
@@ -0,0 +1,28 @@
+//256251735	Apr/12/2024 19:51UTC+6	jahid_hasan_metnick	160A - Twins	C++17 (GCC 7-32)	Accepted	124 ms	0 KB
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long n,m,i,j,sum,cnt,ans;
+    while(cin>>n){
+        long long a[n];
+        sum=0;
+        for(i=0;i<n;i++){
+            cin>>a[i];
+            sum+=a[i];
+        }
+        sum=sum/2;
+        sort(a,a+n);
+        cnt=0;
+        ans=0;
+        for(i=n-1;i>=0;i--){
+            ans+=a[i];
+            cnt++;
+            if(ans>sum)
+                break;
+        }
+        cout<<cnt<<endl;
+    }
+    return 0;
+}
